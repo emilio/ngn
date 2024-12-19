0d1314b12c4b17c738b97dbdb615e178161205a7
Emilio Cobos Álvarez <emilio@crisal.io>
2024-12-20 00:58:57 +0100

    dbus: It discovers peers, and is discoverable.

--- examples/dbus/main.rs
+++ examples/dbus/main.rs
@@ -1,5 +1,5 @@
+use futures_util::StreamExt;
 use std::collections::HashMap;
-
 use tokio;
 use zbus::zvariant::Value;
 
@@ -23,17 +23,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await;
 
-    let iface = {
+    let iface_path = {
         let name = Value::new("wlo2");
         let mut args = HashMap::new();
         args.insert("Ifname", &name);
         proxy.create_interface(args).await?
     };
 
-    dbg!(&iface);
+    dbg!(&iface_path);
 
-    let p2pdevice = wpa_supplicant::p2pdevice::P2PDeviceProxy::new(&conn, iface).await?;
+    // let iface = wpa_supplicant::interface::InterfaceProxy::new(&conn, &iface_path).await?;
+    let p2pdevice = wpa_supplicant::p2pdevice::P2PDeviceProxy::new(&conn, iface_path).await?;
     dbg!(&p2pdevice);
+
+    let cur_config = p2pdevice.p2pdevice_config().await?;
+    dbg!(&cur_config);
+
+    p2pdevice
+        .set_p2pdevice_config({
+            let mut config = HashMap::new();
+            config.insert("DeviceName", "RustTest".into());
+            config.insert("GOIntent", 15u32.into());
+            config
+        })
+        .await?;
+
+    // Start the find operation.
+    p2pdevice.find(HashMap::default()).await?;
+
+    let mut device_found_stream = p2pdevice.receive_device_found().await?;
+    while let Some(msg) = device_found_stream.next().await {
+        dbg!(&msg);
+    }
 
     Ok(())
 }
