b08ad0642bc1df245f65f24ea0784a564b281694
Emilio Cobos Álvarez <emilio@crisal.io>
2025-05-04 23:34:26 +0200

    test: Allow using rr and asan.
    See the commits above for why it's useful. But also useful to debug
    races.

--- test/setup.sh
+++ test/setup.sh
@@ -4,6 +4,9 @@ set -x;
 
 cd "$(dirname "$0")"
 
+DEBUGGER=""
+DEBUGGER="rr"
+
 sudo systemctl stop wpa_supplicant
 sudo systemctl stop NetworkManager
 sudo killall wpa_supplicant
@@ -14,9 +17,22 @@ sudo modprobe mac80211_hwsim radios=3
 
 TMP_DIR=$(mktemp -d)
 
+echo "Test set-up starting, state dir: $TMP_DIR"
+
 # FIXME: Assumes wlan0 is pre-existing. Arguably not amazing but...
 # TODO: Add wlan3 etc once more stuff is working
-IFACES="wlan1 wlan2"
+IFACES="wlan1 wlan2 wlan3"
+
+BUILD_TARGET=x86_64-unknown-linux-gnu
+# BUILD_MODE="release"
+BUILD_MODE="debug"
+CARGO_FLAGS=""
+if [ $BUILD_MODE = "release" ]; then
+  CARGO_FLAGS="--release"
+fi
+CARGO_FLAGS+=" -Zbuild-std"
+export RUSTFLAGS=-Zsanitizer=address
+cargo build -vv --example dbus $CARGO_FLAGS --target=$BUILD_TARGET || exit 1
 
 for iface in $IFACES; do
   IFACE_DIR="$TMP_DIR/$iface"
@@ -27,9 +43,9 @@ for iface in $IFACES; do
   sleep .5 # TODO: Make this more reliable
   ADDRESS=$(cat "$IFACE_DIR/dbus/log" | head -1)
   echo "Address for $iface is $ADDRESS"
-  sudo DBUS_SYSTEM_BUS_ADDRESS=$ADDRESS wpa_supplicant -i $iface -c simple.conf -u >"$IFACE_DIR/wpa_supplicant.log" 2>&1 &
+  sudo DBUS_SYSTEM_BUS_ADDRESS=$ADDRESS $DEBUGGER wpa_supplicant -i $iface -c simple.conf -u 2>&1 | tee "$IFACE_DIR/wpa_supplicant.log" &
   sleep .5 # TODO: Make this more reliable
-  DBUS_SYSTEM_BUS_ADDRESS=$ADDRESS cargo run --example dbus > "$IFACE_DIR/client.log" 2>&1 &
+  DBUS_SYSTEM_BUS_ADDRESS=$ADDRESS nohup $DEBUGGER ../target/$BUILD_TARGET\/$BUILD_MODE/examples/dbus 2>&1 | tee "$IFACE_DIR/client.log" &
   sleep .5 # TODO: Make this more reliable
 done
 
